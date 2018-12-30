069caffa07880d57bb924507230b5f7e82fa67fa
focus237 <emarc237@gmail.com>
2018-12-30 03:38:14 +0100

    Poison v2

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
 # Poison
+
+Poison is a simple PHP template engine. Not as powerful as blade but it allows to do basic operations
+.
+
+## Installation
+
+    composer require focus237/poison
+
+## Basic usage
+
+Here is a simple example for rendering a view :
+
+    <?php
+    
+    use Poison\Poison;
+    
+    $renderer = new Poison('views_path', 'cache_path');
+    
+    $renderer->render('home');
+    
+By default the view extension is .poison.php, so the view `home` match the file `home.poison.php`.
+
+## Settings
+
+Here is the default settings of Poison :
+
+`VIEWS_PATH = NULL`
+
+`CACHE_PATH = NULL`
+
+`VIEW_EXTENSION = .poison.php`
+
+You can change these settings with :
+
+    Poison::setViewsPath('views_path');
+    
+    Poison::setCachePath('cache_path');
+    
+    Poison::setViewExtension('view_extension');
+    
+## Global variables
+
+Sometimes you need some parameters or variables to be accessible from your views, you can do this. 
+Take our example above :
+
+You can do this by passing your variables in parameters to the `render` function
+
+    <?php
+    
+    use Poison\Poison;
+    
+    $renderer = new Poison('views_path', 'cache_path');
+    
+    $name = "John Doe";
+    $items = ["Item 1", "Item 2", "Item 3"];
+    
+    $renderer->render('home', ['name' => $name, 'items' => $items]);
+    
+Or you can use the `addGlobal` function :
+
+    <?php
+    
+    use Poison\Poison;
+    
+    $renderer = new Poison('views_path', 'cache_path');
+    
+    $name = "John Doe";
+    $items = ["Item 1", "Item 2", "Item 3"];
+    
+    $renderer->addGlobal('name', $name)->addGlobal('items', $items)->render('home');
+    
+With the second method, you need to add all your variables before rendering the view.
+
+## Poison/HTML Tags
+
+
+Poison has a tag system that allows you to perform operations that are not available by default with simple HTML. Here's a list of available tags :
+
+    @include('file_to_include_in_html') // Include some poison file in another
+    
+    
+    @extend('view_to_extend', ['key' => 'value'])   // Extend another view with some parameters 
+    
+    
+    @content
+    
+        Some HTML ...
+        
+    @endcontent     // Define the content to pass to a extended view
+    
+    
+    @if(condition)
+    
+        Some HTML ...
+        
+    @elseif(condition)
+    
+        Some HTML ...
+        
+    @else
+    
+        Some HTML ...
+        
+    @endif
+    
+    
+    @foreach($items as $item)
+    
+        {{ $item }}     // Echo $item
+        
+    @endforeach
--- src/Poison.php
+++ src/Poison.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace Poison;
-
+namespace App;
 
 class Poison
 {
 
     private static $viewsPath;
+
+    private static $cachePath;
+
+    private static $viewExtension = '.poison.php';
 
     /**
      * @var array Vars globally accessible for all views
@@ -17,10 +20,13 @@ class Poison
      * Poison constructor.
      * @param string $path Path to the views directory
      */
-    public function __construct(string $path)
+    public function __construct(string $path, string $cache)
     {
         if (!is_null($path))
             self::$viewsPath = $path;
+
+        if (!is_null($cache))
+            self::$cachePath = $cache;
     }
 
     /**
@@ -34,19 +40,174 @@ class Poison
     }
 
     /**
+     * Set cache path
+     * @param string $path Path to the cache directory
+     */
+    public static function setCachePath(string $path): void
+    {
+        if (!is_null($path))
+            self::$cachePath = $path;
+    }
+
+    /**
+     * Change views extension
+     * @param string $extension View extension in the format ".extension"
+     */
+    public static function setViewExtension(string $extension): void
+    {
+        if (!is_null($extension))
+            self::$viewExtension = $extension;
+    }
+
+    /**
+     * Clear the views cache folder
+     */
+    private function clearCache(): void
+    {
+        $files = scandir(self::$cachePath);
+
+        if (count($files) >= 8) {
+            foreach ($files as $file) {
+                if ($file != '.' && $file != '..')
+                    unlink(self::$cachePath . DIRECTORY_SEPARATOR . $file);
+            }
+        }
+    }
+
+    private function parseTag(string $tag): string
+    {
+        $real = $tag;
+
+        if (startsWith($tag, '@include')) {
+            $real = str_replace("@include", "<?= \$poison->include", $tag);
+            $real = $real . '; ?>';
+        }
+
+        if (startsWith($tag, '{{') && endsWith($tag, '}}')) {
+            $real = str_replace("{{", "<?=", $tag);
+            $real = str_replace("}}", ";?>", $real);
+        }
+
+        if (startsWith($tag, '@url')) {
+            $real = str_replace("@url", "<?= \$router->url", $tag);
+            $real = str_replace(")", "); ?>", $real);
+        }
+
+        if(startsWith($tag, '@foreach')) {
+            $real = str_replace("@foreach", "<?php foreach", $tag);
+            $real = $real . ": ?>";
+        }
+
+        if(startsWith($tag, '@endforeach')) {
+            $real = str_replace("@endforeach", "<?php endforeach; ?>", $tag);
+        }
+
+        if(startsWith($tag, '@if')) {
+            $real = str_replace("@if", "<?php if", $tag);
+            $real = $real . ": ?>";
+        }
+
+        if(startsWith($tag, '@else')) {
+            $real = str_replace("@else", "<?php else: ?>", $tag);
+        }
+
+        if(startsWith($tag, '@elseif')) {
+            $real = str_replace("@elseif", "<?php elseif", $tag);
+            $real = $real . ": ?>";
+        }
+
+        if(startsWith($tag, '@endif')) {
+            $real = str_replace("@endif", "<?php endif; ?>", $tag);
+        }
+
+        if (startsWith($tag, '{?')) {
+            $real = str_replace("{?", "<?php", $tag);
+        }
+
+        if (startsWith($tag, '?}')) {
+            $real = str_replace("?}", "?>", $tag);
+        }
+
+        if (startsWith($tag, '@extend')) {
+            $real = str_replace('@extend', '', $tag);
+            $real = str_replace('(', '[', $real);
+            $real = str_replace(')', ']', $real);
+            $real = "<?php \$extension=" . $real;
+            $real = $real . "; ?>";
+            $real = str_replace("\"", "'", $real);
+        }
+
+        if (startsWith($tag, '@content')) {
+            $real = str_replace("@content", "<?php ob_start(); ?>", $tag);
+        }
+
+        if (startsWith($tag, '@endcontent')) {
+            $real = str_replace("@endcontent", "<?php \$extension[1]['content'] = ob_get_clean(); \$poison->render(\$extension[0], \$extension[1]); ?>", $tag);
+        }
+
+        return $real;
+    }
+
+    /**
+     * Create a formatted PHP file and store it to the cache folder
+     * @param string $contents Unformatted PHP Contents
+     * @param array $params Parameters to include in the view
+     * @return string Formatted output for the web browser
+     */
+    private function parseTemplate(string $contents, array $params = []): string
+    {
+
+        preg_match_all("#(\{[@?!])(.*){1,}([@?!]\})|(\@\w+)([\(]*(.+)+?[\)]*)|(\{{2}(.*){1,}\}{2})|(\{[@?!]+)|([@?!]+\})#imxU", $contents, $matches);
+
+        if (count($matches) > 0)
+        {
+
+            $tags = $matches[0];
+
+            foreach ($tags as $tag):
+
+                $real = $this->parseTag($tag);
+
+                $contents = str_replace($tag, $real, $contents);
+
+            endforeach;
+
+            $cachedFile = self::$cachePath . DIRECTORY_SEPARATOR . uniqid("poison.") . '.php';
+
+            file_put_contents($cachedFile, $contents);
+
+            ob_start();
+
+            extract($this->globals);
+            extract($params);
+            require($cachedFile);
+
+            return ob_get_clean();
+
+        } else {
+            return $contents;
+        }
+
+    }
+
+    /**
      * Render a view
      * @param string $view View to render
-     * @param array $params Parameters to pass to the view
+     * @param array $params Parameters to include in the view
      */
     public function render(string $view, array $params = []): void
     {
-        $path = self::$viewsPath . DIRECTORY_SEPARATOR . $view . '.php';
-         ob_start();
-         $poison = $this;
-         extract($this->globals);
-         extract($params);
-         require($path);
-         echo ob_get_clean();
+        $this->clearCache();
+
+        $view = str_replace('.', DIRECTORY_SEPARATOR, $view);
+        $path = self::$viewsPath . DIRECTORY_SEPARATOR . $view . self::$viewExtension;
+
+        if (file_exists($path)) {
+            $output = $this->parseTemplate(file_get_contents($path), $params);
+            echo $output;
+        } else {
+            throw new \Exception("View [{$view}] not found.");
+        }
     }
 
     /**
@@ -54,9 +215,28 @@ class Poison
      * @param string $key Key of the variable to add
      * @param $value Value of the variable to add
      */
-    public function addGlobal(string $key, $value): void
+    public function addGlobal(string $key, $value)
     {
         $this->globals[$key] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Include a file to the view to render
+     * @param string $file File to include in the final ouput
+     */
+    public function include(string $file): void
+    {
+        $file = str_replace('.', DIRECTORY_SEPARATOR, $file);
+        $path = self::$viewsPath . DIRECTORY_SEPARATOR . $file . self::$viewExtension;
+
+        if (file_exists($path)) {
+            $output = $this->parseTemplate(file_get_contents($path));
+            echo $output;
+        } else {
+            throw new \Exception("File [{$file}] not found.");
+        }
     }
 
 }
